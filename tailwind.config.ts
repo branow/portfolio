7156c9c3cdf267395d7950695527df03c79785fc
branow <bodnarorestdraw@gmail.com>
2024-02-02 20:24:17 +0200

    add daisyui module

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui")
+  ],
+  dayisyui: {
+    themes: ["winter"],
+  },
 };
 export default config;
