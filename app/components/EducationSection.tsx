399327d6c8f6477bb801a80b51a38eb3a1a300c0
branow <bodnarorestdraw@gmail.com>
2024-02-04 22:59:56 +0200

    add EducationSection.tsx

---
+++ app/components/EducationSection.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Section from "./Section";
+import Timeline from "./Timeline";
+import { education } from "./../../public/education";
+
+const EducationSection = () => {
+  return (
+    <>
+      <Section
+        title="Education"
+        content={
+          <div>
+            <Timeline events={education} />
+          </div>
+        }
+      />
+    </>
+  );
+};
+
+export default EducationSection;
---
+++ public/education.tsx
@@ -0,0 +1,14 @@
+
+export const education = [
+  {
+    'title': 'Economics: Junior Specialist',
+    'year': '2019-2021',
+    'description': 'Separated Structural Unit Khmelnytskyi Polytechnic Professional College by Lviv Polytechnic National University'
+  },
+  {
+    'title': 'Computer Science: Bachelor',
+    'year': '2021-2025',
+    'description': 'National Technical University of Ukraine “Igor Sikorsky Kyiv Polytechnic Institute”'
+  },
+];
+
