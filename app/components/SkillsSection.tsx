a1ed532ae887accebee0fb8000277bccbbd530c9
branow <bodnarorestdraw@gmail.com>
2024-02-04 14:36:42 +0200

    add SkillsSection.tsx

---
+++ app/components/SkillsSection.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Section from './Section'
+import SkillCard from './SkillCard'
+import java from './../../public/skills/java.png'
+import spring from './../../public/skills/spring.png'
+import javafx from './../../public/skills/javafx.png'
+import mysql from './../../public/skills/mysql.png'
+import git from './../../public/skills/git.png'
+import reactjs from './../../public/skills/reactjs.png'
+import nextjs from './../../public/skills/nextjs.png'
+import tailwind from './../../public/skills/tailwind.png'
+
+
+
+const SkillsSection = () => {
+  return (
+    <>
+    <Section
+      title="Skills"
+      content={
+        <div className='flex flex-row gap-4 flex-wrap'>
+          <SkillCard title="Java SE" score={75} image={java}/>
+          <SkillCard title="Spring Framowork" score={30} image={spring}/>
+          <SkillCard title="JavaFX UI" score={50} image={javafx}/>
+          <SkillCard title="MySQL RDMS" score={80} image={mysql}/>
+          <SkillCard title="Git VCS" score={70} image={git}/>
+          <SkillCard title="React.js" score={45} image={reactjs}/>
+          <SkillCard title="Next.js" score={30} image={nextjs}/>
+          <SkillCard title="Tailwind CSS" score={30} image={tailwind}/>
+        </div>
+      }
+    />
+  </>
+  )
+}
+
+export default SkillsSection
