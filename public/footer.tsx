c7c06dcb42153075803fff8a7f2570e7dd53d142
branow <bodnarorestdraw@gmail.com>
2024-02-09 21:47:08 +0200

    add Footer component

---
+++ public/footer.tsx
@@ -0,0 +1,53 @@
+export const footerColumns = [
+  {
+    title: 'Contact',
+    links: [
+      {
+        lable: 'Email',
+        href: 'mailto:bodnarorestdraw@gmail.com'
+      },
+      {
+        lable: 'LinkedIn',
+        href: 'https://www.linkedin.com/in/orest-bodnar'
+      },
+      {
+        lable: 'UpWork',
+        href: 'https://www.upwork.com/freelancers/~017873b23eb5e9bba6'
+      },
+    ]
+  },
+  {
+    title: 'Social Media',
+    links: [
+      {
+        lable: 'Facebook',
+        href: 'https://www.facebook.com/profile.php?id=61556098941984'
+      },
+      {
+        lable: 'Instagram',
+        href: 'https://www.instagram.com/_branow_/'
+      },
+      {
+        lable: 'Twitter',
+        href: 'https://twitter.com/_branow_'
+      },
+    ]
+  },
+  {
+    title: 'More',
+    links: [
+      {
+        lable: 'Email',
+        href: '/#about'
+      },
+      {
+        lable: 'Portfolio',
+        href: '/#portfolio'
+      },
+      {
+        lable: 'Education',
+        href: '/#education'
+      },
+    ]
+  },
+];
