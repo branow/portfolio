00b1b99be088903ad5db075f5deac1ccca5cf0b4
branow <bodnarorestdraw@gmail.com>
2024-02-13 13:23:04 +0200

    modify layout: add image backgorund, navbar and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { NavBar } from "./components/NavBar/NavBar";
+import Image from "next/image";
+import background from "../public/grey-bubbles.jpg"
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +19,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="winter">
+      <body className={inter.className}>
+        <NavBar />
+        <Image className='fixed top-0 right-0 z-[-1] w-full h-full blur-2xl' alt='grey bubbles' src={background} />
+        <div className="mt-[5rem] w-[90vw] mx-auto lg:w-[85vw] lg:mt-[4.5rem] xl:w-[70vw] 2xl:w-[60vw]">
+          {children}
+        </div>
+        <div className="h-14"></div>
+        <Footer />
+      </body>
     </html>
   );
 }
